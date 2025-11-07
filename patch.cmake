0ca10b9dc94fd60ca2916a67536037286881cd05
xiongli870110-hue <xiongli870110@gmail.com>
2025-11-07 15:59:31 +0800

    Update patch.cmake

--- patch.cmake
+++ patch.cmake
@@ -1,8 +1,8 @@
 target_link_libraries(ttyd
-  PRIVATE /opt/libwebsockets/lib/libwebsockets.a
-  PRIVATE /opt/libuv/lib/libuv.a
-  PRIVATE /opt/json-c/lib/libjson-c.a
-  PRIVATE /opt/openssl/lib/libssl.a
-  PRIVATE /opt/openssl/lib/libcrypto.a
-  PRIVATE /usr/lib/x86_64-linux-gnu/libz.a
-  PRIVATE pthread)
+  /opt/libwebsockets/lib/libwebsockets.a
+  /opt/libuv/lib/libuv.a
+  /opt/json-c/lib/libjson-c.a
+  /opt/openssl/lib/libssl.a
+  /opt/openssl/lib/libcrypto.a
+  /usr/lib/x86_64-linux-gnu/libz.a
+  pthread)
